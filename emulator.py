4502573995e08b3396c76f17ed06992184c25182
Danneh <danneh@danneh.net>
2011-08-14 01:14:45 +1000

    8918 compatability added

--- emulator.py
+++ emulator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from microprocessors.unsw4917 import processor
+#from microprocessors.unsw4917 import processor
+from microprocessors.unsw8918 import processor
 
 custom_processor = processor()
 custom_processor.initialise()
-custom_processor.load_program('example.mc')
-print(custom_processor.memory_locations)
+
+#custom_processor.load_program('examples/countdown.4917')
+custom_processor.load_program('examples/countdown.8918')
+
+#print(custom_processor.memory_locations)
 
 custom_processor.run_program()
---
+++ microprocessors/unsw8918.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from .unsw4917 import processor as microprocessor
+
+class processor(microprocessor):
+    name = 'unsw8918'
+    
+    def __init__(self):
+        microprocessor.__init__(self)
+        self.bitlength = 8
+        self.instruction_codes['24'] = (self.ins_print_ascii, 2)
+    
+    def ins_print(self):
+        print(self.memory_locations[self.registers['ip'] - 1], end='')
+    
+    def ins_print_ascii(self):
+        print(chr(self.memory_locations[self.registers['ip'] - 1]), end='')
